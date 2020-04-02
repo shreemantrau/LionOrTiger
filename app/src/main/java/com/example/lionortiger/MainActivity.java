62b333ba862666f4d5bf700c6d53f3238bd3c8fa
Shreyas <shreyas.karandikar@gmail.com>
2020-04-02 16:12:52 -0700

    onClick functionality added

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lionortiger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void imageViewIsClicked(View tappedImage){
+        ImageView img=(ImageView) tappedImage;
+        img.setTranslationX(-2000);
+        img.setImageResource(R.drawable.lion);
+        img.animate().translationXBy(2000).rotation(3600).setDuration(2000);
     }
 }
